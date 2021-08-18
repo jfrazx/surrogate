1f304a8506b618599c3ceb52dbd4885642ec0a04
jfrazx <staringblind@gmail.com>
2021-08-18 07:56:50 -0700

    feat: move applicable handler options into global options
    closes #49

--- src/interfaces/handlerOptions.ts
+++ src/interfaces/handlerOptions.ts
@@ -32,34 +32,13 @@ export interface NextParameters<T extends object> extends ProviderParameters<T> 
 
 export interface SurrogateHandlerOptions<T extends object> extends SurrogateGlobalOptions {
   /**
-   * @description Pass Next object to the Surrogate Method
-   *
-   * @default true
-   */
-  useNext?: boolean;
-
-  /**
-   * @description Specify that nothing should be passed to handler
-   *
-   * @default false
-   */
-  noArgs?: boolean;
-
-  /**
-   * @description Should errors be thrown or ignored when passed via next()?
-   *
-   * @default false
-   */
-  ignoreErrors?: boolean;
-
-  /**
    * @description Specifies the method context wrapper to utilize
    *
    * @options
-   *  - none
+   *  - sync
    *  - async
    *
-   * @default none
+   * @default sync
    */
   wrapper?: MethodWrappers;
 
